ffd07c4293e7e28ee327c4469c156c0a1690aea4
Erika A <54790753+erikaatmaja@users.noreply.github.com>
2021-05-20 03:18:54 +0700

    fragments dah jalan

--- app/src/main/java/jennie/umn/ac/testregisfirebase/MainActivity.java
+++ app/src/main/java/jennie/umn/ac/testregisfirebase/MainActivity.java
@@ -19,7 +19,7 @@ import jennie.umn.ac.testregisfirebase.Fragments.SearchFragment;
 
 import static jennie.umn.ac.testregisfirebase.R.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     BottomNavigationView bottomNavigationView;
     Fragment selectedFragment = null;
@@ -29,47 +29,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        loadFragment(new HomeFragment());
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         getSupportFragmentManager().beginTransaction().replace(id.fragment_container, new HomeFragment()).commit();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            switch (item.getItemId()){
-                case id.nav_home:
-                    selectedFragment = new HomeFragment();
-                    break;
-
-                case id.nav_search:
-                    selectedFragment = new SearchFragment();
-                    break;
-
-                case id.nav_comm:
-                    selectedFragment = new CommissionFragment();
-                    break;
-
-                case id.nav_post:
-                    selectedFragment = null;
-                    startActivity(new Intent(MainActivity.this, PostActivity.class));
-                    break;
-                case id.nav_profile:
-                    SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
-                    editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
-                    editor.apply();
-                    selectedFragment = new ProfileFragment();
-                    break;
-            }
-            if(selectedFragment != null){
-                getSupportFragmentManager().beginTransaction().replace(id.fragment_container, selectedFragment).commit();
-            }
-
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(id.fragment_container, fragment)
+                    .commit();
             return true;
         }
-    };
+        return false;
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_search:
+                fragment = new CommissionFragment();
+                break;
+            case R.id.nav_comm:
+                fragment = new ProfileFragment();
+                break;
+            case id.nav_post:
+                selectedFragment = null;
+                startActivity(new Intent(MainActivity.this, PostActivity.class));
+                break;
+            case id.nav_profile:
+                SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                editor.apply();
+                selectedFragment = new ProfileFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
+    }
 }
--- app/src/main/java/jennie/umn/ac/testregisfirebase/PostActivity.java
+++ app/src/main/java/jennie/umn/ac/testregisfirebase/PostActivity.java
@@ -1,9 +1,5 @@
 package jennie.umn.ac.testregisfirebase;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -16,6 +12,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,11 +23,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.util.HashMap;
 
